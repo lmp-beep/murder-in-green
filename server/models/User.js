29a20ae9d8a0d446b73aa044244e107268f8bdf7
Lisa Pessin <lisapessin@gmail.com>
2021-07-20 21:04:34 -0400

    Users to User

---
+++ server/models/User.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// import schema from Survey.js
+const Survey = require('./Survey');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+ 
+    user: [Survey],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 6;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // custom method to compare and validate password for logging in
+  userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+  
+  // when we query a user, we'll also get another field called `saveResults` with the number of saved books we have
+  // userSchema.virtual('userAnswers').get(function () {
+  //   return this.Survey.answers;
+  // });
+  
+  const User = model('User', userSchema);
+  
+  module.exports = User;
